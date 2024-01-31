fea07ff90cc5ed19c2c5495b5807e8c5226a40c0
Kevin Chu <chu.kevin.h@gmail.com>
2024-01-31 15:40:10 +1100

    Added cat gif for entertainment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react"
 import './style.css';
+import catTyping from './images/cat-typing.gif'
+import catSad from './images/sad-cat.gif'
+import catInit from './images/cat-typing-still.jpg'
 
 
 export default function App() {
@@ -7,13 +10,32 @@ export default function App() {
     //const [totalTime, setTotalTime] = React.useState([])
     const [timeStudiedToday, setTimeStudiedToday] = React.useState(parseInt(localStorage.getItem("studyTime")) || 0)
     const [currTime, setCurrTime] = React.useState("")
-    const [study, setStudy] = React.useState(false)
+    const [study, setStudy] = React.useState(
+        {
+            init: true,
+            isStudy: false
+        }
+    )
+    const [timeStudied, setTimeStudied] = React.useState(
+        {
+            hours: Math.floor(timeStudiedToday/3600),
+            minutes: Math.floor((timeStudiedToday/60)%60), 
+            seconds: Math.floor(timeStudiedToday%60)
+        }
+    )
 
     React.useEffect(() => {
         const interval = setInterval(() => getTime(), 1000)
 
-        if (study === true) {
+        if (study.isStudy === true) {
             setTimeStudiedToday(prevState => prevState + 1)
+            setTimeStudied(
+                {
+                    hours: Math.floor(timeStudiedToday/3600),
+                    minutes: Math.floor((timeStudiedToday/60)%60), 
+                    seconds: Math.floor(timeStudiedToday%60)
+                }
+            )
             localStorage.setItem("studyTime", timeStudiedToday)
         }
     }, [currTime])
@@ -23,7 +45,20 @@ export default function App() {
     }
 
     function handleClick() {
-        setStudy(prevState => !prevState)
+        setStudy(prevState => {
+            return {
+                ...prevState,
+                isStudy: !prevState.isStudy
+            }
+        })
+        if (study.init === true) {
+            setStudy(prevState => {
+                return {
+                    ...prevState,
+                    init: !prevState.init
+                }
+            })
+        }
     }
 
     return (
@@ -31,10 +66,13 @@ export default function App() {
             <h1>Current time: {currTime}</h1>
             <div>
                 <button onClick={handleClick}>
-                    <div>{study === false ? "Start Studying" : "Stop Studying"}</div>
+                    <div>{study.isStudy === false ? "Start Studying" : "Stop Studying"}</div>
                 </button>
             </div>
-            <h1>Time spent studying: {timeStudiedToday} seconds</h1>
+            <h1>Time spend studying: {timeStudiedToday} seconds</h1>
+            {/* <h1>Time spend Studying: {timeStudied.hours} hours, {timeStudied.minutes} minutes, {timeStudied.seconds} seconds</h1> */}
+            <br/>
+            <img src={study.init === true ? catInit : (study.isStudy === true ? catTyping : catSad)} className="img-cat"/>
         </div>
     );
 }
